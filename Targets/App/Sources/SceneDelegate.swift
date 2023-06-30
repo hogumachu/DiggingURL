2b5b0cb39d385ac03728fa0c27edc0a48bbd3b75
hogumachu <castle_jun@daum.net>
2023-06-30 14:14:30 +0900

    chore: setup scene delegate

--- Targets/App/Sources/SceneDelegate.swift
+++ Targets/App/Sources/SceneDelegate.swift
@@ -1,10 +1,21 @@
+import Core
+import Feature
 import UIKit
+import Swinject
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    private var coordinator: AppCoordinator?
+    private let injector: DependencyInjector = DependencyInjectorImpl(container: Container())
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = scene as? UIWindowScene else { return }
+        let window = UIWindow(windowScene: scene)
+        self.window = window
+        window.makeKeyAndVisible()
+        setupDependencies()
+        coordinator?.start(root: .root)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
@@ -12,5 +23,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {}
     func sceneWillEnterForeground(_ scene: UIScene) {}
     func sceneDidEnterBackground(_ scene: UIScene) {}
+    
+}
+
+extension SceneDelegate {
+    
+    private func setupDependencies() {
+        let factory = SceneFactoryImp(injector: injector)
+        let coordinator = AppCoordinatorImp(factory: factory)
+        self.coordinator = coordinator
+        
+        // TODO: - Assemble & Register (Injector)
+    }
     
 }
